8f75e6cbfd8ae344d5bfcf2f544cb869312c9a45
HERDEVLAN <herdevlanx@gmail.com>
2025-03-31 23:06:47 -0400

    Proyecto base

---
+++ js/script.js
@@ -0,0 +1,6 @@
+// Obtener elementos del DOM
+constverDetallesButton=document.getElementById('verDetalles');
+constvolverButton=document.getElementById('volver');
+consttarjeta=document.querySelector('.tarjeta');
+// Función para voltear la tarjeta
+// Función para volver a la cara frontal
